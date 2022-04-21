eae7aaff04b43545acbcbbfa0fe9907b31f7a32c
iron <satls34@naver.com>
2022-04-21 21:24:44 +0900

    feat : ContextUtil 클래스 내에 dp를 px로 변경해주는 공통 메서드 추가

--- Util/src/main/java/com/iron/util/ContextUtil.kt
+++ Util/src/main/java/com/iron/util/ContextUtil.kt
@@ -1,8 +1,22 @@
 package com.iron.util
 
+import android.content.Context
+import android.util.TypedValue
+import androidx.annotation.DimenRes
+
 /**
-* @author 최철훈
-* @created 2022-04-21
-* @desc 
-*/class ContextUtil {
+ * @author 최철훈
+ * @created 2022-04-21
+ * @desc Util 클래스
+ */
+object ContextUtil {
+
+    fun numberToPx(context: Context, dp: Int) =
+        TypedValue.applyDimension(
+            TypedValue.COMPLEX_UNIT_DIP,
+            dp.toFloat(), context.resources.displayMetrics
+        ).toInt()
+
+    fun dpToPx(context: Context, @DimenRes id: Int) =
+        context.resources.getDimensionPixelSize(id)
 }
