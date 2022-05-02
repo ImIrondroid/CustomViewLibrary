a7b262a83d9701cca10ffca1f5524806d6a705c0
iron <satls34@naver.com>
2022-05-02 23:37:10 +0900

    feat : ArcProgressBar (반원 형태) 개발

--- app/src/main/java/com/iron/libary/MainActivity.kt
+++ app/src/main/java/com/iron/libary/MainActivity.kt
@@ -1,11 +1,50 @@
 package com.iron.libary
 
+import android.animation.ObjectAnimator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.iron.libary.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.button1.setOnClickListener {
+            moveProgress(0, 5000)
+        }
+
+        binding.button2.setOnClickListener {
+            moveProgress(5000, 10000)
+        }
+
+        binding.button3.setOnClickListener {
+            moveProgress(10000, 5000)
+        }
+
+        binding.button4.setOnClickListener {
+            moveProgress(5000, 0)
+        }
+    }
+
+    private fun moveProgress(
+        nowProgress: Int,
+        nextProgress: Int
+    ) {
+        binding.arcProgressBar.run {
+            val animationTest = ObjectAnimator.ofInt(
+                binding.arcProgressBar,
+                "progress",
+                nowProgress, nextProgress
+            )
+
+            animationTest.apply {
+                duration = 500
+            }.start()
+        }
     }
 }
